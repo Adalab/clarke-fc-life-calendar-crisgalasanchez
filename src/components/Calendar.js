5d643a2d53c8da64f206d29c2bdb34b4f01a1fdf
cristina <crisgalasanchez@gmail.com>
2018-03-21 13:47:00 +0100

    create file of components

---
+++ src/components/Calendar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {Render} from 'react-dom';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+
+
+class Calendar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="calendar">
+      <p> smiley  </p>
+      </div>
+    );
+  }
+}
+
+export default Calendar;
---
+++ src/components/Editor.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+
+
+class Editor extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="editor">
+         <form>
+            <div>
+              <label for="calendary">Fecha</label>
+              <input type="date" name="day" id="calendary" value="" />
+            </div>
+            <div>
+              <label>Estado</label>
+              <input type="radio" name="smiley" value="good" checked/> :)
+              <input type="radio" name="smiley" value="bad" /> :(
+            </div>
+            <div>
+              <label for="message">Mensaje</label>
+              <textarea name="message" id="message" rows="2" cols="30" placeholder="¿Por qué es un buen día?"></textarea>
+            </div>
+            <button className="buttonSave">Guardar</button>
+            <button className="buttonCancel">Cancelar</button>
+          </form>
+       </div>
+    );
+  }
+}
+
+export default Editor;
